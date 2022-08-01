22e9795d3d0ff92cb3826d090621bd65f0655dc7
Laptop <babalielchin@gmail.com>
2022-08-01 19:08:32 +0400

    added some requierements with bugs

--- client/src/common/actions.js
+++ client/src/common/actions.js
@@ -4,8 +4,55 @@ const GET_MESSAGES = gql`
     subscription {
         messages {
             id
-            content
             user
+            content
+            likes
+            dislikes
+            replies {
+                user
+                content
+            }
+        }
+    }
+`;
+
+const LIKE_SUBSCRIPTION = gql`
+  subscription Like($user: String!, $messageId: String!) {
+    like(user: $user, messageId: $messageId) {
+        likes
+        dislikes
+    }
+  }
+`;
+
+const DISLIKE_SUBSCRIPTION = gql`
+  subscription Dislike($user: String!, $messageId: String!) {
+    dislike(user: $user, messageId: $messageId) {
+        likes
+        dislikes
+    }
+  }
+`;
+
+const REPLY_SUBSCRIPTION = gql`
+  subscription Reply($messageId: String!) {
+      reply(messageId: $messageId) {
+          replies {
+              user
+              content
+          }
+      }
+  }
+`;
+
+const GET_MESSAGE = gql`
+    subscription {
+        like {
+            id
+            user
+            content
+            likes
+            dislikes
         }
     }
 `;
@@ -16,4 +63,32 @@ const POST_MESSAGE = gql`
     }
 `;
 
-export { GET_MESSAGES, POST_MESSAGE };
+const POST_USER = gql`
+    mutation {
+        postUser
+    }
+`;
+
+const POST_LIKE = gql`
+    mutation ($user:String!, $messageId:String!) {
+        postLike(user: $user, messageId: $messageId)
+    }
+`;
+
+const POST_DISLIKE = gql`
+    mutation ($user:String!, $messageId:String!) {
+        postDislike(user: $user, messageId: $messageId)
+    }
+`;
+
+const POST_REPLY = gql`
+    mutation ($user:String!, $messageId:String!, $content:String!) {
+        postReply(user: $user, messageId: $messageId, content: $content) {
+            id
+            user
+            content
+        }
+    }
+`;
+
+export { GET_MESSAGES, POST_MESSAGE, POST_USER, POST_LIKE, POST_DISLIKE, GET_MESSAGE, LIKE_SUBSCRIPTION, DISLIKE_SUBSCRIPTION, POST_REPLY, REPLY_SUBSCRIPTION };
--- server/server.js
+++ server/server.js
@@ -1,12 +1,27 @@
 const { GraphQLServer, PubSub } = require('graphql-yoga');
 
+const users = [];
 const messages = [];
 
 const typeDefs = `
+    type User {
+        id: ID!
+    }
+
+    type Reply {
+        id: ID!
+        user: String!
+        content: String!
+        messageId: String!
+    }
+
     type Message {
         id: ID!
         user: String!
         content: String!
+        likes: [String!]
+        dislikes: [String!]
+        replies: [Reply!]
     }
 
     type Query {
@@ -15,10 +30,17 @@ const typeDefs = `
 
     type Mutation {
         postMessage(user: String!, content: String!): ID!
+        postUser: ID!
+        postLike(user: String!, messageId: String!): Int!
+        postDislike(user: String!, messageId: String!): Int!
+        postReply(user: String!, messageId: String!, content: String!): Message!
     }
 
     type Subscription {
         messages: [Message!]
+        like(user: String!, messageId: String!): Message!
+        dislike(user: String!, messageId: String!): Message!
+        reply(messageId: String!): Message!
     }
 `;
 
@@ -28,17 +50,79 @@ const onMessagesUpdates = (fn) => subscribers.push(fn);
 const resolvers = {
     Query: {
         messages: () => messages,
+        // user: (id) => users.find(user => user === id)
     },
     Mutation: {
-        postMessage: (parent, {user, content}) => {
+        postUser: (parent, {}) => {
+            const id = users.length;
+            users.push(id.toString());
+            return id;
+        },
+        postMessage: (parent, { user, content }) => {
             const id = messages.length;
             messages.push({
                 id,
                 content,
-                user
+                user,
+                likes: [],
+                dislikes: [],
+                replies: []
             });
             subscribers.forEach((fn) => fn());
             return id;
+        },
+        postLike: (parent, { user, messageId }) => {
+            const message = messages.find((mess) => mess.id == messageId);
+            if (message.likes.includes(user)) {
+                message.likes = message.likes.filter(liker => liker !== user);
+                
+                pubsub.publish('LIKE', message);
+                return 0;
+            }
+            else if (message.dislikes.includes(user)) {
+                message.dislikes = message.dislikes.filter(disliker => disliker !== user);
+                message.likes.push(user);
+
+                pubsub.publish('LIKE', message);
+                return 1;
+            }
+            else {
+                message.likes.push(user);
+
+                pubsub.publish('LIKE', message);
+                return 1;
+            }
+         },
+        postDislike: (parent, { user, messageId }) => {
+            const message = messages.find((mess) => mess.id == messageId);
+            if (message.dislikes.includes(user)) {
+                message.dislikes = message.dislikes.filter(disliker => disliker !== user);
+
+                pubsub.publish('DISLIKE', message);
+                return 0;
+            }
+            else if (message.likes.includes(user)) {
+                message.likes = message.likes.filter(liker => liker !== user);
+                message.dislikes.push(user);
+
+                pubsub.publish('DISLIKE', message);
+                return 1;
+            }
+            else {
+                message.dislikes.push(user);
+
+                pubsub.publish('DISLIKE', message);
+                return 1;
+            }
+        },
+        postReply: (parent, { user, messageId, content }) => {
+            const message = messages.find(mes => mes.id == messageId);
+            message.replies.push({
+                user,
+                content
+            });
+            pubsub.publish('REPLY', message);
+            return message;
         }
     },
     Subscription: {
@@ -49,6 +133,18 @@ const resolvers = {
                 setTimeout(() => pubsub.publish(channel, { messages }), 0);
                 return pubsub.asyncIterator(channel);
             }
+        },
+        like: {
+            subscribe: (parent, args, { pubsub }) => pubsub.asyncIterator('LIKE'),
+            resolve: payload => payload
+        },
+        dislike: {
+            subscribe: (parent, args, { pubsub }) => pubsub.asyncIterator('DISLIKE'),
+            resolve: payload => payload
+        },
+        reply: {
+            subscribe: (parent, args, { pubsub }) => pubsub.asyncIterator('REPLY'),
+            resolve: payload => payload
         }
     }
 }
